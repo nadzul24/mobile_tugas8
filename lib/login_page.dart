4575d4b4f21ae43bc2c1466e17f5a9ac2d2ff566
= <=>
2021-04-26 13:24:20 +0700

    tugas 8_no register

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,6 +8,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +24,133 @@ class _LoginPageState extends State<LoginPage> {
             children: <Widget>[
               FlutterLogo(size: 150),
               SizedBox(height: 50),
+              _formLogin(),
               _signInButton(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _formLogin() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Email',
+            style: TextStyle(color: Colors.black54),
+          ),
+          SizedBox(height: 8),
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'Enter Email',
+              fillColor: Colors.white,
+              filled: true,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.black26,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.black54,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Password',
+            style: TextStyle(color: Colors.black54),
+          ),
+          SizedBox(height: 8),
+          TextFormField(
+            controller: _passController,
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Enter Password',
+              fillColor: Colors.white,
+              filled: true,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.black26,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.black54,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  signInWithEmailAndPassword(
+                          _emailController.text, _passController.text)
+                      .then((result) {
+                    if (result != null) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return FirstScreen();
+                          },
+                        ),
+                      );
+                    }
+                  });
+                }
+              },
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              color: Color(0xFF4f4f4f),
+              elevation: 0,
+              padding: EdgeInsets.symmetric(vertical: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+    SizedBox(height: 20);
+    Row(
+      children: <Widget>[
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: Colors.black54,
+          ),
+        ),
+        SizedBox(width: 20),
+        Text(
+          'OR',
+          style: TextStyle(color: Colors.black54),
+        ),
+        SizedBox(width: 20),
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: Colors.black54,
+          ),
+        ),
+      ],
     );
   }
 
@@ -53,7 +179,6 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(
